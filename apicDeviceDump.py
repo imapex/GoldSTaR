ef223503a57dda529135401869afb9a05a725deb
briansak <brsak@cisco.com>
2017-04-18 07:51:41 -0500

    Added a menu and organized the data into lists

--- apicDeviceDump.py
+++ apicDeviceDump.py
@@ -8,7 +8,10 @@ import json
 #import getpass to mask password entries
 import getpass
 
+#define variables
 controller='devnetapi.cisco.com/sandbox/apic_em'
+allPlatformIds=[]  #List to hold PlatformIDs
+runningVersions=[]  #List to hold tuple items (PlatformIDs, Software Versions)
 
 def getTicket(apic_username, apic_password):
     # put the ip address or dns of your apic-em controller in this url
@@ -33,6 +36,10 @@ def getTicket(apic_username, apic_password):
 
 
 def getNetworkDevices(ticket):
+    #Define Globals
+    global allPlatformIds
+    global runningVersions
+
     # URL for network device REST API call to get list of existing devices on the network.
     url = "https://" + controller + "/api/v1/network-device"
 
@@ -50,13 +57,11 @@ def getNetworkDevices(ticket):
   #convert data to json format.
     r_json=response.json()
 
-    allPlatformIds=[]  #List to hold PlatformIDs
-    runningVersions={}  #Dictionary to hold PlatformIDs, Software Versions
   #Iterate through network device data and print the id and series name of each device
     for i in r_json["response"]:
-        print(i["hostname"] + "   " + i["platformId"] + "   " + i["softwareVersion"])
+        #print(i["hostname"] + "   " + i["platformId"] + "   " + i["softwareVersion"])
         allPlatformIds.append(str(i["platformId"]))
-        runningVersions[str(i["platformId"])]=str(i["softwareVersion"])
+        runningVersions.append((str(i["platformId"]), str(i["softwareVersion"])))
 
 def suggestedSoftware():
     #Placeholder to house Suggested Software API code
@@ -64,6 +69,9 @@ def suggestedSoftware():
     print suggestedSoftwareVersion
 
 def main():
+    #define Globals
+    global allPlatformIds
+    global runningVersions
 
     print "*************************************"
     print "*        Welcome to GoldSTaR        *"
@@ -74,6 +82,19 @@ def main():
     #call the functions
     theTicket=getTicket(apic_username,apic_password)
     getNetworkDevices(theTicket)
+
+    print "What Would You Like to Do?\n"
+    print "1) Display Collected Platform IDs"
+    print "2) Display Running Software Versions\n"
+
+    menuSelection = raw_input("Selection >> ")
+
+    if menuSelection == "1":
+        print '\n'.join(str(p) for p in allPlatformIds)
+    elif menuSelection == "2":
+        print '\n'.join(str(p) for p in runningVersions)
+    else:
+        print "My God, Man! There's only 2 choices, you can't pick one of those?"
 
 #Start the App
 main()
