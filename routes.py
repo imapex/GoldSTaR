983a0a305fd2ce2a873bb568ae70b027f3d25c57
briansak <brsak@cisco.com>
2017-04-25 19:41:28 -0500

    Authentication and Inventory work at a rudimentary level in Flask.

--- routes.py
+++ routes.py
@@ -2,13 +2,33 @@ from flask import Flask, request, render_template
 import jinja2
 from app import app
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	if request.method == 'GET':
+		return render_template('index.html')
+	elif request.method == 'POST':
+		apic_username = request.form['apic_username']
+		apic_password = request.form['apic_password']
+		pi_username = request.form['pi_username']
+		pi_password = request.form['pi_password']
+
+		if apic_username:
+			from app import getTicket, getDevicesFromAPICEM
+			theTicket=getTicket(apic_username,apic_password)
+			getDevicesFromAPICEM(theTicket)
+		elif pi_username:
+			from app import getDevicesFromPrime
+			getDevicesFromPrime(pi_username,pi_password)
+
+		from app import runningVersions
+		return render_template('inventory.html', inventory = runningVersions)
+	else:
+		return "<h2> Invalid Request </h2>"
 
 @app.route('/inventory')
 def inventory():
-	return render_template('inventory.html')
+	from app import runningVersions
+	return render_template('inventory.html', inventory = runningVersions)
 
 @app.route('/software')
 def software():
